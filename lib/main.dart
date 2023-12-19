a3e9daf180c16b389dcba0dcbb4ca4bfaee3f5a0
fabio-vokrri <fabiovokrri.fv01@gmail.com>
2023-12-19 21:53:47 +0100

    ray tracing first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+void main(List<String> args) async {
+  int width = 256;
+  int heigth = 256;
+
+  StringBuffer content = StringBuffer("P3\n$width $heigth\n255\n");
+
+  for (int i = 0; i < heigth; i++) {
+    for (int j = 0; j < width; j++) {
+      int r = (i / (width - 1) * 255.999).toInt();
+      int g = (j / (heigth - 1) * 255.999).toInt();
+      int b = 0;
+
+      content.write("$r $g $b\n");
+    }
+  }
+
+  File image = File("output_image.ppm");
+  await image.writeAsString(content.toString());
+}
