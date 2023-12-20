222902929a13b034b2f60806c08bf5d8d782e63b
fabio-vokrri <fabiovokrri.fv01@gmail.com>
2023-12-20 09:50:14 +0100

    added ray class and documentation

---
+++ lib/utility/ray.dart
@@ -0,0 +1,24 @@
+import 'package:ray_tracing/utility/vector.dart';
+
+/// Ray type: represents a ray in space
+class Ray {
+  final Point3 _origin;
+  final Vector3 _direction;
+
+  const Ray({
+    required Vector3 origin,
+    required Vector3 direction,
+  })  : _origin = origin,
+        _direction = direction;
+
+  /// Returns the origin point of this ray.
+  Point3 get origin => _origin;
+
+  /// Returns the direction vector of this ray.
+  Vector3 get direction => _direction;
+
+  /// Returns the point along this ray at distance `t` from the origin.
+  Point3 at(double t) {
+    return origin + direction * t;
+  }
+}
--- lib/utility/vector.dart
+++ lib/utility/vector.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+/// Vector3 type: represents a three-dimensional vector.
 class Vector3 {
   final double _x;
   final double _y;
@@ -19,14 +20,22 @@ class Vector3 {
   double get y => _y;
   double get z => _z;
 
+  /// Returns the vector length, defined as the vector modulus.
   double get length => sqrt(squaredLength);
+
+  /// Returns the vector length squared.
   double get squaredLength => _x * _x + _y * _y + _z * _z;
 
+  /// Returns a new vector with unit modulus (whose length is 1) from this vector.
+  ///
+  /// Returns this vector if its length is zero or one.
   Vector3 get normalized {
-    if (length == 0) return this;
+    if (length == 0 || length == 1) return this;
     return this / length;
   }
 
+  /// Returns a new vector whose components are the sum between 
+  /// the components of this vectori and the ones of `other`.
   Vector3 operator +(Vector3 other) {
     return Vector3(
       _x + other.x,
@@ -35,6 +44,8 @@ class Vector3 {
     );
   }
 
+  /// Returns a new vector whose components are the difference between
+  /// the components of this vector and the ones of `other`.
   Vector3 operator -(Vector3 other) {
     return Vector3(
       _x - other.x,
@@ -43,6 +54,8 @@ class Vector3 {
     );
   }
 
+  /// Returns a new vector whose components are the negated version
+  /// of the components of this vector.
   Vector3 operator -() {
     return Vector3(
       -_x,
@@ -51,6 +64,8 @@ class Vector3 {
     );
   }
 
+  /// Returns a new vector whose components are generated from the product
+  /// of every component of this vector by `t`.
   Vector3 operator *(num t) {
     return Vector3(
       _x * t,
@@ -59,6 +74,10 @@ class Vector3 {
     );
   }
 
+  /// Returns a new vector whose components are generated from the division
+  /// of every component of this vector by `t`.
+  ///
+  /// Throws `ArgumentException` if the given number is 0.
   Vector3 operator /(num t) {
     if (t == 0) {
       throw ArgumentError("Division by zero not supoorted");
@@ -66,6 +85,8 @@ class Vector3 {
     return this * (1 / t);
   }
 
+  /// Returns a new vector generated from the product of every component
+  /// of this vector with the corresponding component of `other`.
   Vector3 multiplyBy(Vector3 other) {
     return Vector3(
       x * other.x,
@@ -74,6 +95,8 @@ class Vector3 {
     );
   }
 
+  /// Returns a new vector whose components are generated from the cross product
+  /// between this vector and `other`.
   Vector3 cross(Vector3 other) {
     return Vector3(
       _y * other.z - _z * other.y,
@@ -82,9 +105,13 @@ class Vector3 {
     );
   }
 
+  /// Returns the dot product between this vector and `other`.
   double dot(Vector3 other) {
     return _x * other.x + _y * other.y + _z * other.z;
   }
 }
 
+/// Point3 type: represents a point a three-dimensional space.
+/// A point in space can be represented as a vector having its
+/// tail at (0, 0, 0) and its tip at the corresponding point.
 typedef Point3 = Vector3;
